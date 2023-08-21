4aec5ff416fe6f09a59800eb70c881bd372e7f0e
Dang <kkdang0103@gmail.com>
2023-08-21 18:32:35 +0700

    6th commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 const path =  require('path');
 const express = require('express');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
-// const hbs = handlebars.create();
+const hbs = handlebars.create({extname: '.hbs'});
 
 app.use(morgan('combined'));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('hbs', hbs.engine);
 
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
 app.get('/home', (req, res) => {
