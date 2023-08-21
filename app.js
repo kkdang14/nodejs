ade41c03d817c49b89d818d2bc3c053fb9a730df
Dang <kkdang0103@gmail.com>
2023-08-21 10:14:48 +0700

    3th commit

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const morgan = requre('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/trang', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c= a + b;
     res.send('Hello World!')
 })
 
