da9d4c6e07411be8cf98fe1c57d46d1fd06c0927
Dang <kkdang0103@gmail.com>
2023-08-23 19:06:17 +0700

    update 8232023

--- src/app.js
+++ src/app.js
@@ -7,11 +7,14 @@ const port = 3000;
 const hbs = handlebars.create({extname: '.hbs'});
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded())
+app.use(express.json())
 
-app.use(morgan('combined'));
+//HTTP Logger
+// app.use(morgan('combined'));
 
+//Template engine
 app.engine('hbs', hbs.engine);
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -23,6 +26,14 @@ app.get('/news', (req, res) => {
     res.render('news');
 })
 
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('');
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
